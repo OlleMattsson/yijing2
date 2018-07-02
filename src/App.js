db1fefd6bdcdd4f84d4682c8125ed6f3a2d94337
Olle Mattsson <olle.mattsson@gmail.com>
2018-07-02 08:17:01 +0300

    css transition tet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from "react";
 import "./App.css";
 import Hexagram from "./Hexagram";
+import { CSSTransition } from "react-transition-group"; // ES6
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayHej: false,
+      transition: false
+    };
+
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    this.setState(p => ({ displayHej: !p.displayHej, transition: true }));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
-        <Hexagram withControls />
-        <Hexagram fuxi={7} />
         <Hexagram
           fuxi={25}
-          changing={[true, false, false, false, false, false]}
+          changing={[true, false, true, false, false, true]}
           interactive
+          withControls
         />
+
+        <CSSTransition
+          classNames="example"
+          timeout={{ exit: 0, enter: 300 }}
+          unmountOnExit
+          in={this.state.displayHej && !this.state.transition}
+          onExited={() => this.setState(p => ({ transition: false }))}
+        >
+          <h1>Dirp</h1>
+        </CSSTransition>
+
+        <CSSTransition
+          classNames="example"
+          timeout={{ exit: 0, enter: 300 }}
+          unmountOnExit
+          in={!this.state.displayHej && !this.state.transition}
+          onExited={() => this.setState(p => ({ transition: false }))}
+        >
+          <h1>Durp =)</h1>
+        </CSSTransition>
+
+        <button onClick={this.clickHandler}>state</button>
       </div>
     );
   }
--- src/components/line/Yin.js
+++ src/components/line/Yin.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Yin.css";
 
 export default class Yin extends Component {
   render() {
