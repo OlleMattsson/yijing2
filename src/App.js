5c2e8ed0c5999644f83d169448c1d76b31403da0
Olle Mattsson <olle.mattsson@gmail.com>
2019-06-20 02:02:38 +0300

    hexagram refactored to function component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Hexagram from "./components/Hexagram";
+import {Hexagram} from "./components/Hexagram";
 import {
   makeLineWithFourCoins,
   makeFutureHexagram,
--- src/components/Hexagram.js
+++ src/components/Hexagram.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Line from "./line/Line";
 import { Yin } from "./line/Yin";
 import { Yang } from "./line/Yang";
@@ -11,82 +11,71 @@ import {
 } from "../lib/iching-helpers";
 import HexagramData from "./HexagramData";
 
-export default class Hexagram extends Component {
-  constructor(props) {
-    super(props);
+export const Hexagram = ({
+  fuxi,
+  interactive,
+  changing = new Array(6).fill(false),
+  withControls
+}) => {
+  const [sequence, setSequence] = useState(
+    binaryToBool(fuxiToBinary(fuxi || 0)) || new Array(6).fill(false) // broken: true / false
+  );
 
-    this.state = {
-      fuxi: this.props.fuxi || 0,
-      sequence:
-        binaryToBool(fuxiToBinary(this.props.fuxi)) || new Array(6).fill(false), // broken: true / false
-      changing: this.props.changing || new Array(6).fill(false),
-      interactive: false,
-      ...props
-    };
-  }
-
-  handleLineClick = index => {
-    const sequence = this.state.sequence;
-    sequence[index] = !sequence[index];
-    this.setState({
-      sequence: sequence
-    });
+  const handleLineClick = index => {
+    const _sequence = [...sequence];
+    _sequence[index] = !_sequence[index];
+    setSequence(_sequence);
   };
 
-  handlePrevClick = () => {
-    const prev = binaryToFuxi(this.state.sequence) - 1;
+  const handlePrevClick = () => {
+    const prev = binaryToFuxi(sequence) - 1;
     const prevBinary = fuxiToBinary(prev);
-    this.setState({ sequence: binaryToBool(prevBinary) });
+    setSequence(binaryToBool(prevBinary));
   };
 
-  handleNextClick = () => {
-    const next = binaryToFuxi(this.state.sequence) + 1;
+  const handleNextClick = () => {
+    const next = binaryToFuxi(sequence) + 1;
     const nextBinary = fuxiToBinary(next);
-    this.setState({ sequence: binaryToBool(nextBinary) });
+    setSequence(binaryToBool(nextBinary));
   };
 
-  render() {
-    const sequenceCopy = Array.from(this.state.sequence);
-    const changingCopy = Array.from(this.state.changing).reverse();
+  // do the reversing first, to avoid flickering
+  const reversedSequence = Array.from(sequence).reverse();
+  const reversedChanging = Array.from(changing).reverse();
 
-    const lines = sequenceCopy.reverse().map((state, index) => {
-      const id = this.state.sequence.length - 1 - index;
-      return (
-        <Line
-          id={id}
-          key={id}
-          broken={state}
-          changing={changingCopy[index]}
-          yin={props => <Yin {...props} />}
-          yang={props => <Yang {...props} />}
-          color={globals.lineColor}
-          changingColor={globals.lineColorChanging}
-          onChange={this.state.interactive ? this.handleLineClick : () => {}}
-        />
-      );
-    });
-
-    const controls = (
-      <div>
-        <button onClick={this.handlePrevClick}>&lt; &lt;</button>
-        <button onClick={this.handleNextClick}>&gt; &gt;</button>
-      </div>
-    );
-
+  const lines = reversedSequence.map((state, index) => {
+    const id = sequence.length - 1 - index;
     return (
-      <div className="hexagram" style={{ flex: 1, margin: "10px" }}>
-        {lines}
-        <div className="fuxiLabel">
-          Fu Xi binary: {binaryToFuxi(this.state.sequence)}
-        </div>
-        <div className="kingwenLabel">
-          King Wen: {binaryToKingWen(this.state.sequence)}
-        </div>
-
-        {this.props.withControls ? controls : null}
-
-        <HexagramData fuxi={binaryToFuxi(this.state.sequence)} />
-      </div>
+      <Line
+        id={id}
+        key={id}
+        broken={state}
+        changing={reversedChanging[index]}
+        yin={props => <Yin {...props} />}
+        yang={props => <Yang {...props} />}
+        color={globals.lineColor}
+        changingColor={globals.lineColorChanging}
+        onChange={interactive ? handleLineClick : () => {}}
+      />
     );
-  }
-}
+  });
+
+  const controls = (
+    <div>
+      <button onClick={handlePrevClick}>&lt; &lt;</button>
+      <button onClick={handleNextClick}>&gt; &gt;</button>
+    </div>
+  );
+
+  return (
+    <div className="hexagram" style={{ flex: 1, margin: "10px" }}>
+      {lines}
+      <div className="fuxiLabel">Fu Xi binary: {binaryToFuxi(sequence)}</div>
+      <div className="kingwenLabel">King Wen: {binaryToKingWen(sequence)}</div>
+
+      {withControls ? controls : null}
+
+      <HexagramData fuxi={binaryToFuxi(sequence)} />
+    </div>
+  );
+};
