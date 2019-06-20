3af263e173aaad598b2b97c69d0caf7e14be224c
Olle Mattsson <olle.mattsson@gmail.com>
2019-06-20 14:53:41 +0300

    fix color bug in yin line

--- src/components/line/Yin.js
+++ src/components/line/Yin.js
@@ -5,7 +5,7 @@ export const Yin = ({ isChanging, color, changingColor }) => {
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="300" height="25">
-      <line x2="120" stroke={color} strokeWidth="50" />
+      <line x2="120" stroke={strokeColor} strokeWidth="50" />
       <line x1="180" x2="300" stroke={strokeColor} strokeWidth="50" />
     </svg>
   );
